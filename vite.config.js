c1e7d4ee33c2307009eef394de3827075d0cb0f6
Surajkumar9913 <surajyuva200@gmail.com>
2024-09-18 14:50:06 +0530

    proxy added

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     server: {
-      proxy: {
-        '/api': {
-          target: 'https://fullstack-crud-app-2vyb.onrender.com',
-          changeOrigin: true,
-          secure: false,
+        proxy: {
+            // "/api": "https://fullstack-crud-app-2vyb.onrender.com",
         },
-      },
     },
     plugins: [react()],
-  });
-  
+});
